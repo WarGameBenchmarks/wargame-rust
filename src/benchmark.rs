db762be7dea110b172e1619f91c4469792f47875
Ryan Rampersad <ryan.rampersad@gmail.com>
2015-07-07 01:16:51 -0500

    Added stats

--- src/benchmark.rs
+++ src/benchmark.rs
@@ -47,7 +47,7 @@ pub fn benchmark(tasks: usize) {
                     Ok(r) => {if r == 1 {break;}}
                     Err(_) => {}
                 }
-                
+
             }
 
             let _ = tr_tx.send(task_id as u32);
@@ -59,6 +59,8 @@ pub fn benchmark(tasks: usize) {
 
     // TODO: split the calculations into its own method
 
+    let mut samples = Vec::with_capacity(100);
+
     let mut phase = 1u32;
 
     let mut total_games = 0u64;
@@ -67,10 +69,16 @@ pub fn benchmark(tasks: usize) {
     let mut test_duration = 0f64;
 
     // 1 minute in nanoseconds
-    let prime_time = 60000000000;
+    // let prime_time = 60000000000;
+    let prime_time = 15000000000;
     let maximum_tests = 100u32;
     let percent_variation = 0.0001f64;
+
     let display_frequency = 50000000u64;
+    let sample_frequency = 10000000u64;
+
+    let mut last_display_time = 0u64;
+    let mut last_sample_time = 0u64;
 
     let ms = 1000000u64;
     let ns = 1000000000u64;
@@ -78,7 +86,6 @@ pub fn benchmark(tasks: usize) {
     let mut tests = 0;
 
     let mut elapsed_time;
-    let mut last_time = 0u64;
     let mut test_time = 0u64;
 
     let mut speed:f64;
@@ -91,11 +98,15 @@ pub fn benchmark(tasks: usize) {
 
     let mut test_started = false;
 
+    let mut mean;
+    let mut stdev;
+    let mut cov = 0f64;
+
     // the monitor loop collects the total game count
     // and the elasped time so far
     println!("\n{}. prime time has started", phase);
     'monitor: loop {
-        
+
         /*
             Query each counter
         */
@@ -111,13 +122,14 @@ pub fn benchmark(tasks: usize) {
         current_time = precise_time_ns();
         elapsed_time = current_time - start_time;
 
-        
+
         // NANOSECONDS per GAME
         rate = elapsed_time as f64 / total_games as f64;
 
         // GAMES per NANOSECOND
         speed = 1f64 / rate as f64;
         speed_v = speed * ms as f64;
+
 
         // the priming phase
         if !test_started && elapsed_time >= prime_time {
@@ -127,54 +139,62 @@ pub fn benchmark(tasks: usize) {
             println!("\n{}. stability testing has started", phase);
         } else if test_started && elapsed_time >= test_time {
 
-            // testing phase
-            if rate_low < rate && rate < rate_high || tests >= maximum_tests {
-                // end the monitor infinite loop
-                println!("\n{}. stability testing has ended", phase);
-                break 'monitor;
-            } else {
-                // calculate the details for the next testing phase
-                
-                // ceil does not need the +1 because 0..1 will always yeild 1
-                test_duration = f64::ceil(f64::sqrt(speed_v));
-                test_time = elapsed_time + (test_duration * ns as f64) as u64;
-                
-                percent_rate = rate * percent_variation;
+            mean = get_mean(&samples);
+            stdev = get_standard_deviation(&samples, mean);
+            cov = get_coefficient_of_variation(mean, stdev);
 
-                rate_low = rate - percent_rate;
-                rate_high = rate + percent_rate;
-                tests = tests + 1;
-            }
+           if cov <= 1f64 || tests >= maximum_tests {
+               println!("\n{}. stability testing has ended", phase);
+               break 'monitor;
+           } else {
+               test_duration = 1f64;
+               test_time = elapsed_time + (test_duration * ns as f64) as u64;
+           }
 
+           tests = tests + 1;
+        }
+
+        if (current_time - last_sample_time) > sample_frequency {
+            last_sample_time = current_time;
+            samples.push(speed_v);
         }
 
         /*
             If the time between prints is .5s, print out an updated string.
         */
-        if  (current_time - last_time) > display_frequency {
-            last_time = current_time;
+        if  (current_time - last_display_time) > display_frequency {
+            last_display_time = current_time;
             if phase == 1 {
                 backprint(
-                        format!("{}. et = {}s; g = {}; s = {sv:.5} g/ms;\t", 
+                        format!("{}. et = {}s; g = {}; s = {sv:.5} g/ms;\t",
                             phase,
                             elapsed_time / ns,
                             total_games,
                             sv=speed_v
                         )
                     );
-            } 
+            }
         else {
+            // backprint(
+            //         format!("{}. et = {}s; g = {}; s = {sv:.5} g/ms; t = {t}; \t",
+            //             phase,
+            //             elapsed_time / ns,
+            //             total_games,
+            //             sv=speed_v,
+            //             t = format!(
+            //                 "{} @ {}s",
+            //                     tests,
+            //                     test_duration
+            //                 )
+            //         )
+            //     );
             backprint(
-                    format!("{}. et = {}s; g = {}; s = {sv:.5} g/ms; t = {t}; \t", 
+                    format!("{}. et = {}s; g = {}; s = {sv:.5} g/ms; cov = {cov:.5}; \t",
                         phase,
                         elapsed_time / ns,
-                        total_games, 
-                        sv=speed_v, 
-                        t = format!(
-                            "{} @ {}s",
-                                tests,
-                                test_duration
-                            )
+                        total_games,
+                        sv=speed_v,
+                        cov = cov
                     )
                 );
         }
@@ -184,7 +204,7 @@ pub fn benchmark(tasks: usize) {
     }
 
     // TODO: split the thread cleanup into its own method
-    
+
     for i in 0..tasks {
         let _ = terminate_senders[i].send(1);
     }
@@ -206,10 +226,57 @@ pub fn benchmark(tasks: usize) {
 
     // show results
     println!("\n\n\t\t----\n");
+
+
+    let mean = get_mean(&samples);
+
+    println!("Samples: {} collected", samples.len());
+    println!("Mean: {}", mean);
+
+    let stdev = get_standard_deviation(&samples, mean);
+
+    println!("Standard Deviation: {}", stdev);
+
+    let coefficient_of_variation = get_coefficient_of_variation(mean, stdev);
+
+    println!("Coefficient of Variation: {}%", coefficient_of_variation);
+
+    let standard_error = get_standard_error(&samples, mean);
+
+    println!("Standard Error: {}", standard_error);
+
+
+    println!("\n\n\t\t----\n");
     println!("Threads: {}", tasks);
     println!("Speed: {sv:.5}", sv = speed_v);
     println!("Total Games: {}", total_games);
     println!("Elapsed Time: {} nanoseconds; {} seconds", elapsed_time, elapsed_time / ns);
     println!("Score: {}", f64::round(speed_v));
 
+}
+
+fn get_standard_error(samples: &[f64], mean: f64) -> f64 {
+    return mean / (samples.len() as f64).sqrt();
+}
+
+fn get_coefficient_of_variation(mean: f64, stdev: f64) -> f64 {
+    return (stdev / mean) * 100 as f64;
+}
+
+fn get_standard_deviation(samples: &[f64], mean: f64) -> f64 {
+    let mut total_stdev = 0f64;
+    for i in 0..samples.len() {
+        total_stdev = total_stdev + (samples[i] - mean).powi(2);
+    }
+    let stdev = (total_stdev / samples.len() as f64).sqrt();
+    return stdev;
+}
+
+fn get_mean(samples: &[f64]) -> f64 {
+    let mut total_mean:f64 = 0f64;
+    for i in 0..samples.len() {
+        total_mean = total_mean + samples[i];
+    }
+    let mean = total_mean / samples.len() as f64;
+    return mean;
 }
